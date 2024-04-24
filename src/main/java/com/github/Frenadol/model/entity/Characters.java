79d9e9c79e4e7bba92f1edc137eb11de4a5fadc6
Frenadol <you@example.com>
2024-04-24 18:22:55 +0200

    (Probando que e lgithub funciona bien)

--- src/main/java/com/github/Frenadol/model/entity/Characters.java
+++ src/main/java/com/github/Frenadol/model/entity/Characters.java
@@ -2,6 +2,7 @@ package com.github.Frenadol.model.entity;
 
 
 import java.util.HashSet;
+import java.util.Objects;
 
 public class Characters {
     private int id_character;
@@ -13,4 +14,119 @@ public class Characters {
     private String UltraSuperAttack;
     private Rarety rarety;
     private String passive;
+    public Characters(){
+
+    }
+
+    public Characters(int id_character, Type type, Class character_class, String name, HashSet<String> categories, String superAttack, String ultraSuperAttack, Rarety rarety, String passive) {
+        this.id_character = id_character;
+        this.type = type;
+        this.character_class = character_class;
+        this.name = name;
+        this.categories = categories;
+        SuperAttack = superAttack;
+        UltraSuperAttack = ultraSuperAttack;
+        this.rarety = rarety;
+        this.passive = passive;
+    }
+
+    public int getId_character() {
+        return id_character;
+    }
+
+    public void setId_character(int id_character) {
+        this.id_character = id_character;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Class getCharacter_class() {
+        return character_class;
+    }
+
+    public void setCharacter_class(Class character_class) {
+        this.character_class = character_class;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HashSet<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(HashSet<String> categories) {
+        this.categories = categories;
+    }
+
+    public String getSuperAttack() {
+        return SuperAttack;
+    }
+
+    public void setSuperAttack(String superAttack) {
+        SuperAttack = superAttack;
+    }
+
+    public String getUltraSuperAttack() {
+        return UltraSuperAttack;
+    }
+
+    public void setUltraSuperAttack(String ultraSuperAttack) {
+        UltraSuperAttack = ultraSuperAttack;
+    }
+
+    public Rarety getRarety() {
+        return rarety;
+    }
+
+    public void setRarety(Rarety rarety) {
+        this.rarety = rarety;
+    }
+
+    public String getPassive() {
+        return passive;
+    }
+
+    public void setPassive(String passive) {
+        this.passive = passive;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Characters that = (Characters) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Characters{" +
+                "id_character=" + id_character +
+                ", type=" + type +
+                ", character_class=" + character_class +
+                ", name='" + name + '\'' +
+                ", categories=" + categories +
+                ", SuperAttack='" + SuperAttack + '\'' +
+                ", UltraSuperAttack='" + UltraSuperAttack + '\'' +
+                ", rarety=" + rarety +
+                ", passive='" + passive + '\'' +
+                '}';
+    }
 }
